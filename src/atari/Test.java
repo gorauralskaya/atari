56f4af6afb68930a03903767ea7e6dd2b540b7e4
gorauralskaya <gorauralskaya@gmail.com>
2016-08-24 17:21:48 +0500

    1

--- src/atari/Test.java
+++ src/atari/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("Hello world!");
+System.out.print("Goodbye null world!");
+
 	}
 
 }
