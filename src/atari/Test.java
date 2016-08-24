389a09b1954ba263a9e79df47e48b21713d5d73b
gorauralskaya <gorauralskaya@gmail.com>
2016-08-24 17:26:59 +0500

    indian circle

--- src/atari/Test.java
+++ src/atari/Test.java
@@ -5,7 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("Hello world!");
-System.out.print("Goodbye null world!");
+System.out.print("Goodbye null!");
+System.out.print("Hello null!");
+System.out.print("Goodbye world!");
+System.out.print("Hello world!");
+System.out.print("Goodbye null!");
 
 	}
 
